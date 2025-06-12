0cdb285161482ce4de12a93646fdebfe1e0c1dcf
shruthik shetty <shrutikshetty14@gmail.com>
2025-06-12 23:24:28 +0530

    fix lax issue

--- src/commons/utils/setAccessTokenCookies.ts
+++ src/commons/utils/setAccessTokenCookies.ts
@@ -13,14 +13,14 @@ export const setAccessTokenCookies = (
   res.cookie('accessToken', accessToken, {
     httpOnly: false, // normal cookie so JS can read it
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 60 * 60 * 1000, // 60 minutes
   });
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true, // secure from JS access
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
