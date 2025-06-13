611cce84b51b68c8168189519360dd9b5e456b58
shruthik shetty <shrutikshetty14@gmail.com>
2025-06-13 10:41:56 +0530

    env set up

--- src/commons/utils/setAccessTokenCookies.ts
+++ src/commons/utils/setAccessTokenCookies.ts
@@ -10,19 +10,19 @@ export const setAccessTokenCookies = (
   accessToken: string,
   refreshToken: string
 ): void => {
-  const isProduction = process.env.NODE_ENV === 'production';
+  const secureCookie = process.env.COOKIE_SECURE === 'true';
 
   res.cookie('accessToken', accessToken, {
     httpOnly: false, // normal cookie so JS can read it
-    secure: isProduction,
-    sameSite: isProduction ? 'none' : 'lax',
+    secure: secureCookie,
+    sameSite: secureCookie ? 'none' : 'lax',
     maxAge: 60 * 60 * 1000, // 60 minutes
   });
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true, // secure from JS access
-    secure: isProduction,
-    sameSite: isProduction ? 'none' : 'lax',
+    secure: secureCookie,
+    sameSite: secureCookie ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
