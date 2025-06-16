6f912086a74364b69e91fd93c129884219c665a7
shruthik shetty <shrutikshetty14@gmail.com>
2025-06-16 12:46:50 +0530

    mentor route

--- src/controllers/mentor.controller.ts
+++ src/controllers/mentor.controller.ts
@@ -3,20 +3,33 @@
  */
 
 import { handleError } from '../commons/utils/handleError';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import User from '../models/user.model';
 import { ValidatedRequest } from '../types/custom-types';
+import { Types } from 'mongoose';
 
 /**
  * Controller to get all mentors
  */
 export const getMentors = async (
-  _: ValidatedRequest<undefined>,
+  req: ValidatedRequest<undefined>,
   res: Response
 ) => {
   try {
+    //get the start and limit query params
+    const query = (req as unknown as Request).query;
+
+    const start = Math.max(0, parseInt(query.start as string, 10) || 0);
+
+    const limit = Math.max(1, parseInt(query.limit as string, 10) || 100);
+
     //get the mentors from Users model
-    const mentors = await User.find({ mentor: true }).select("-password -__v -createdAt -updatedAt -role").lean().exec();
+    const mentors = await User.find({ mentor: true })
+      .select('-password -__v -createdAt -updatedAt -role')
+      .skip(start)
+      .limit(limit)
+      .lean()
+      .exec();
 
     // in case no mentors found
     if (!mentors || mentors.length === 0) {
@@ -28,6 +41,55 @@ export const getMentors = async (
     res.status(200).json({ success: true, data: mentors });
   } catch (err) {
     //handle unexpected error
+    handleError(res, { error: err });
+  }
+};
+
+/**
+ * Controller to get a mentor by id (this is nothing but user id)
+ * this will only return the user details if he is a mentor
+ */
+export const getMentorById = async (
+  req: ValidatedRequest<undefined>,
+  res: Response
+) => {
+  try {
+    // get the mentor id from params
+    const { mentorId } = req.params;
+
+    // validate if the mentor id is a valid mongo id
+    if (!mentorId || !Types.ObjectId.isValid(mentorId)) {
+      handleError(res, {
+        statusCode: 400,
+        message: 'Mentor id is empty or invalid',
+      });
+      return;
+    }
+
+    // check if the user is a mentor
+    const user = await User.findById(mentorId)
+      .select('-password -__v -createdAt -updatedAt -role')
+      .lean()
+      .exec();
+
+    if (!user) {
+      handleError(res, {
+        statusCode: 404,
+        message: 'User not found',
+      });
+      return;
+    } else if (!user.mentor) {
+      handleError(res, {
+        statusCode: 403,
+        message: 'User is not a mentor',
+      });
+      return;
+    }
+
+    // send response with the mentor
+    res.status(200).json({ success: true, data: user });
+  } catch (err) {
+    // handle unexpected error
     handleError(res, { error: err });
   }
 };
--- src/routes/mentor.route.ts
+++ src/routes/mentor.route.ts
@@ -4,13 +4,16 @@
 
 import { Router } from 'express';
 import { requireUser } from '../commons/middlewares/authorizeUser';
-import { getMentors } from '../controllers/mentor.controller';
+import { getMentors, getMentorById } from '../controllers/mentor.controller';
 
 // initialize router
 const router = Router();
 
 // Route to get all mentors
 router.get('/', requireUser, getMentors);
+
+// get mentor by id
+router.get('/:mentorId', requireUser, getMentorById);
 
 // export all the routes clubbed
 export default router;
